04e55208d5ef791f89845b0c5deab9861b662b38
Simeon Radivoev <simeonradivoev@gmail.com>
2018-03-27 22:45:40 +0300

    Added travis build file * Added build method

---
+++ Assets/Plugins/UniGit/Editor/Utils/GitCiBuild.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace UniGit.Utils
+{
+	public class GitCiBuild
+	{
+		[UsedImplicitly]
+		static void PerformBuild()
+		{
+			string dllDir = GetArg("-dlldir");
+			if (!Directory.Exists(dllDir)) Directory.CreateDirectory(dllDir);
+			else
+			{
+				Directory.Delete(dllDir,true);
+				Directory.CreateDirectory(dllDir);
+			}
+
+			List<string> filesToCopy = new List<string>
+			{
+				Application.dataPath.Replace("Assets", UniGitPath.Combine("Library", "ScriptAssemblies", "UniGit.dll")), 
+				Application.dataPath.Replace("Assets", UniGitPath.Combine("Library", "ScriptAssemblies", "UniGit.dll.mdb")), 
+				UniGitPath.Combine(Application.dataPath, "Plugins", "UniGit", "Editor", "UniGitResources.dll"),
+				UniGitPath.Combine(Application.dataPath, "Plugins", "UniGit", "Editor", "UniGitResources.dll.mdb"),
+				UniGitPath.Combine(Application.dataPath, "Plugins", "UniGit", "Editor", "UniGitResources.pdb")
+			};
+
+			foreach (var file in filesToCopy)
+			{
+				File.Copy(file,Path.Combine(dllDir,Path.GetFileName(file)),true);
+			}
+		}
+
+		private static string GetArg(string name)
+		{
+			var args = System.Environment.GetCommandLineArgs();
+			for (int i = 0; i < args.Length; i++)
+			{
+				if (args[i] == name && args.Length > i + 1)
+				{
+					return args[i + 1];
+				}
+			}
+			return null;
+		}
+	}
+}
